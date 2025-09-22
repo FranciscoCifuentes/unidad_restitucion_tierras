a852b41e18eee09485e6f1775518789ded315eb8
Francisco Cifuentes <f.acifuentes@hotmail.com>
2025-09-21 22:02:59 -0500

    fix: :bug: Correccion de lenguaje en los filtros
    Se corrige el lenguaje utilizado en los filtros de la tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { primengEs } from '../primeng-locale.es';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'unidad_restitucion_tierras';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primengConfig.setTranslation(primengEs);
+  }
 }
